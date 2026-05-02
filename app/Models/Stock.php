812a905054a3e5d3ad2f1bfdb0c91fd5a809401e
fahim-86 <famambission491@gmail.com>
2026-05-02 20:58:53 +1000

    Change Stock model, add two array object fillable & casts

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -3,16 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Stock extends Model
 {
-    use HasFactory;
+    protected $fillable = ['trading_code', 'ltp', 'fetched_at'];
 
-
-    protected $fillable = [
-        'trading_code',
-        'ltp',
-        'last_fetched_at',
+    protected $casts = [
+        'ltp'        => 'float',
+        'fetched_at' => 'datetime',
     ];
 }
